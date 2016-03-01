33075a93538bd881e00fd0f98a1194b620457500
Licong Tang <litang@rim.net>
2016-03-01 18:54:40 -0500

    Update login

--- validation/signup.php
+++ validation/signup.php
@@ -3,21 +3,21 @@ include_once '../database.php';
 include_once 'tokenService.php';
 include_once 'encryptPassword.php';
 
-$username = isset($_GET['username']) ? $_GET['username'] : '';
+$email = isset($_GET['email']) ? $_GET['email'] : '';
 $password = isset($_GET['password']) ? $_GET['password'] : '';
 
-$validUsername = validateUsername($username);
-if ($validUsername){
+$validEmail = validateEmail($email);
+if ($validEmail){
     $hash = getHashFromPassword($password);
-    db_query_noreturn("INSERT INTO userprofile (username, hash) VALUES ('$username', '$hash');");
-    createAndSaveTokenForUser($username);
+    db_query_noreturn("INSERT INTO userprofile (email, hash) VALUES ('$email', '$hash');");
+    createAndSaveTokenForUser($email);
     echo "Success!";
 } else {
-    echo "Username Exists!";
+    echo "email Exists!";
 }
 
-function validateUsername($uname){
-    $result = db_query("SELECT 1 FROM userprofile WHERE username = '$uname' LIMIT 1");
+function validateEmail($email){
+    $result = db_query("SELECT 1 FROM userprofile WHERE email = '$email' LIMIT 1");
     $num = $result->num_rows;
 
     if ($num == 0)
